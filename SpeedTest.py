3fc3e752d3aac564b2f69f695e2bd9d2b25cd8c6
TheoNeo Games <76736404+theo-neo-games@users.noreply.github.com>
2021-01-28 15:54:51 +0000

    Add files via upload

---
+++ SpeedTest.py
@@ -0,0 +1,26 @@
+
+import time
+
+
+def Algorithm1():
+    #Insert code for first algorithm here
+    pass
+
+
+def Algorithm2():
+    #Insert code for second algorithm here
+    pass
+
+
+startTime1 = time.time()
+Algorithm1()
+endTime1 = time.time()
+timeTaken1 = endTime1 - startTime1
+
+startTime2 = time.time()
+Algorithm2()
+endTime2 = time.time()
+timeTaken2 = endTime2 - startTime2
+
+print('Algorithm 1 took {0} seconds.'.format(timeTaken1))
+print('Algorithm 2 took {0} seconds.'.format(timeTaken2))
